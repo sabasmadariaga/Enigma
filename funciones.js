c2fe3dae93ecd92f253db4427e5e8cfaeed01175
Sabas Madariaga <122745883+sabasmadariaga@users.noreply.github.com>
2024-02-14 19:35:03 -0300

    Update funciones.js
    Mejore espacios entrefunciones

--- funciones.js
+++ funciones.js
@@ -1,26 +1,20 @@
 function validarMensaje() {
     var mensajeInput = document.getElementById("mensaje").value;
     var mensajeError = document.getElementById("mensajeError");
-
     var caracteresPermitidos = /^[a-z\s]+$/;
-
-    if (!caracteresPermitidos.test(mensajeInput)) {
+        if (!caracteresPermitidos.test(mensajeInput)) {
         mensajeError.innerText = "¡¡¡¡ERROR!!!! Solo se permiten letras minúsculas y espacios en el mensaje.";
-    } else {
-        mensajeError.innerText = "";
+        } else {
+            mensajeError.innerText = "";
     }
 }
 
 function encriptar() {
     var mensaje = document.getElementById("mensaje").value;
-    var resultado = ''; 
-   
-    for (var i = 0; i < mensaje.length; i++) {
-        
-        var caracter = mensaje[i];
-        
-        
-        switch (caracter) {
+    var resultado = '';
+       for (var i = 0; i < mensaje.length; i++) {       
+            var caracter = mensaje[i];       
+                switch (caracter) {
             case 'a':
                 resultado += 'ai';
                 break;
@@ -39,56 +33,49 @@ function encriptar() {
             default:
                 resultado += caracter; 
         }
-    }
-
-    
+    }    
     document.getElementById("resultado").value = resultado;
 }
 function desencriptar() {
     var mensaje = document.getElementById("mensaje").value;
-    var resultado = ''; 
-    
-    
-    for (var i = 0; i < mensaje.length; i++) {
-        
-        var caracter = mensaje[i];
-        
-        
-        switch (caracter) {
-            case 'a':
-                if (mensaje[i + 1] === 'i') {
+    var resultado = '';   
+    for (var i = 0; i < mensaje.length; i++) {  
+        var caracter = mensaje[i];       
+            switch (caracter) {
+                case 'a':
+                    if (mensaje[i + 1] === 'i') {
                     resultado += 'a'; 
                     i += 1; 
-                } else {
+                    } else {
                     resultado += caracter; 
-                }
+                    }
                 break;
-            case 'e':
-                if (mensaje[i + 1] === 'n' && mensaje[i + 2] === 't' && mensaje[i + 3] === 'e' && mensaje[i + 4] === 'r') {
+                case 'e':
+                    if (mensaje[i + 1] === 'n' && mensaje[i + 2] === 't' && mensaje[i + 3] === 'e' && mensaje[i + 4] === 'r') {
                     resultado += 'e'; 
                     i += 4; 
-                } else {
-                    resultado += caracter; 
+                    } else {
+                        resultado += caracter; 
                 }
                 break;
-            case 'i':
-                if (mensaje[i + 1] === 'm' && mensaje[i + 2] === 'e' && mensaje[i + 3] === 's') {
+                case 'i':
+                    if (mensaje[i + 1] === 'm' && mensaje[i + 2] === 'e' && mensaje[i + 3] === 's') {
                     resultado += 'i'; 
                     i += 3; 
-                } else {
+                    } else {
                     resultado += caracter; 
                 }
                 break;            
-            case 'o':
-                if (mensaje[i + 1] === 'b' && mensaje[i + 2] === 'e' && mensaje[i + 3] === 'r') {
+                case 'o':
+                    if (mensaje[i + 1] === 'b' && mensaje[i + 2] === 'e' && mensaje[i + 3] === 'r') {
                     resultado += 'o'; 
                     i += 3; 
                 } else {
                     resultado += caracter; 
                 }
                 break;
-            case 'u':
-                if (mensaje[i + 1] === 'f' && mensaje[i + 2] === 'a' && mensaje[i + 3] === 't') {
+                case 'u':
+                    if (mensaje[i + 1] === 'f' && mensaje[i + 2] === 'a' && mensaje[i + 3] === 't') {
                     resultado += 'u'; 
                     i += 3; 
                 } else {
